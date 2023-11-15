34259139316cccf61e19bab9f1aa09d45572355b
Phai Hoang <phaihoang@Phais-MacBook-Pro.local>
2023-11-15 10:23:00 +0700

    add more tests

--- FeedTests/RemoteFeedLoaderTests.swift
+++ FeedTests/RemoteFeedLoaderTests.swift
@@ -52,11 +52,16 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOnNon200HTTPResponse() {
         let (sut, client) = makeSUT()
 
-        var capturedError = [RemoteFeedLoader.Error]()
-        sut.load() { capturedError.append($0) }
-        client.complete(withStatusCode: 400)
+        let samples = [199, 201, 300, 400, 500]
+        
+        samples.enumerated().forEach { index, code in
+            var capturedError = [RemoteFeedLoader.Error]()
+            sut.load() { capturedError.append($0) }
+            client.complete(withStatusCode: code, at: index)
+        
+            XCTAssertEqual(capturedError, [.invalidData])
 
-        XCTAssertEqual(capturedError, [.invalidData])
+        }
     }
    
     // MARK: Helpers
